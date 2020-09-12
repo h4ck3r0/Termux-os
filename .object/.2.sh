5c3538cf85a12426b46d7e9f5c1067b8535d2242
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2020-09-12 21:51:50 +0530

    Update .2.sh

--- .object/.2.sh
+++ .object/.2.sh
@@ -10,7 +10,7 @@ echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[10
 echo
 echo -e "  \e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
 echo ""
-
+cd ~/Termux-os/.object
 echo -e '\e[1;96m'
 read -p '  Type Banner Name ❯ ' name
 sed -e "s/\PROC/$name/g" .1zshrc > $HOME/.zshrc
