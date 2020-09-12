f55d0e1091623d3a2d8bd4648caf91782dc4e0f9
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2020-09-12 12:50:35 +0530

    Add files via upload

---
+++ .object/.1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+PUT(){ echo -en "\033[${1};${2}H";}
+clear;toilet -t -f mono12  "H4Ck3R" --gay -F border 
+echo ""
+PUT 11 40
+echo -e "\e[92mBy\e[1;93m RajAryan\e[1;95m /\e[1;96m H4Ck3R0 "
+PUT 12 3
+echo
+echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92myoutube.com/h4ck3r_raj\e[0m"
+echo
+echo -e "  \e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
+echo ""
+echo -e '\e[1;96m'
+read -p '  Type Shell Name ❯ ' name
+sed -e "s/\H4ck3r/$name/g" .h4Ck3r.zsh-theme > $HOME/.oh-my-zsh/themes/h4Ck3r.zsh-theme
