c1e3b2756bec6a0dc1d768dc013a7a7173df3d58
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2021-06-08 21:00:17 +0530

    Update .1.sh

--- .object/.1.sh
+++ .object/.1.sh
@@ -13,4 +13,4 @@ echo ""
 cd ~/Termux-os/.object
 echo -e '\e[1;96m'
 read -p '  Type Shell Name ❯ ' name
-sed -e "s/\ PROC/$name/g" .h4Ck3r.zsh-theme > $HOME/.oh-my-zsh/themes/h4Ck3r.zsh-theme
+sed -e "s/\H4ck3r/$name/g" .h4Ck3r.zsh-theme > $HOME/.oh-my-zsh/themes/h4Ck3r.zsh-theme
