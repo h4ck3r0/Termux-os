a80e234c528f21e73b5fc86749f9966a10764931
Raj Aryan <46929618+h4ck3r0@users.noreply.github.com>
2026-01-18 04:55:19 +0530

    added lock system in os.sh
    Refactor os.sh script to streamline setup functions and improve banner display.

--- os.sh
+++ os.sh
@@ -7,7 +7,6 @@ C='\033[1;96m'
 W='\033[1;97m'
 RS='\033[0m'
 
-
 term_width=$(tput cols)
 BOX_WIDTH=$(( term_width > 60 ? 58 : term_width - 2 ))
 margin=$(( (term_width - BOX_WIDTH) / 2 ))
@@ -27,98 +26,82 @@ print_center() {
     printf "${C}${left_pad}║%*s${color}%s${C}%*s║${RS}\n" $space_len "" "$text" $(( BOX_WIDTH - 2 - len - space_len )) ""
 }
 
-
 banner() {
     clear
     echo -e "${R} ________     _____    ______       __    __     __    __   __     __  " 
     echo -e "${R}(___  ___)   / ___/   (    __ \      \ \  / /     ) )  ( (  (_ \   / _) " 
     echo -e "${C}    ) )     ( (__      ) (__) )      () \/ ()     ( (    ) )   \ \_/ /   " 
-    echo -e "${C}   ( (       ) __)    (    __/       / _  _ \      ) )  ( (     \   /    "  
-    echo -e "${B}    ) )     ( (        ) \ \  _     / / \/ \ \    ( (    ) )     / _ \    " 
-    echo -e "${B}   ( (       \ \___   ( ( \ \_))   /_/      \_\    ) \__/ (    _/ / \ \_  " 
-    echo -e "${G}   /__\       \____\   )_) \__/   (/          \)  \______/  (__/   \__)"
+    echo -e "${C}   ( (       ) __)    (    __/       / _  _ \      ) )  ( (     \   /     "  
+    echo -e "${B}    ) )     ( (        ) \ \  _      / / \/ \ \     ( (    ) )     / _ \    " 
+    echo -e "${B}   ( (       \ \___   ( ( \ \_))    /_/      \_\     ) \__/ (     _/ / \ \_  " 
+    echo -e "${G}   /__\       \____\   )_) \__/    (/          \)  \______/  (__/   \__)"
     echo -e ""
     echo -e "${left_pad}${R}Youtube${C} / ${W}youtube.com/h4ck3r0"
     echo -e "${left_pad}${R}Github${C} / ${Y}H4Ck3R0"
-    echo -e "${left_pad}${R}Telegram${C} / ${G}H4Ck3R_0"
+    echo -e "${left_pad}${R}Telegram${C} / ${G}H4Ck3_R0"
     echo ""
     draw_line "╔" "╗"
     print_center "[√] by Raj Aryan / youtube.com/h4ck3r0" "$G"
     draw_line "╚" "╝"
 }
 
-
-1line() {
-    apt update && apt upgrade
-    pkg install zsh -y
-    pkg install git -y
-    pkg install figlet toilet -y
-    pkg install ruby -y
-    pkg install wget -y
-    gem install lolcat 
-    pkg install curl -y
-    pkg install zsh -y
+1line() { apt update && apt upgrade; pkg install zsh git figlet toilet ruby wget curl -y; gem install lolcat; clear; cd ~/Termux-os/.object/ && cp -r 'ANSI Shadow.flf' $PREFIX/share/figlet/ASCII-Shadow.flf; git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh; pkg install toilet figlet exa -y; cd ~/Termux-os/.object; rm -rf ~/.termux/colors.properties; rm -rf /data/data/com.termux/files/usr/etc/motd; cp -r .colors.properties ~/.termux/colors.properties; cp -r .termux.properties ~/.termux.properties; curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf > ~/.termux/font.ttf; clear; cd ~/Termux-os ; bash os.sh; termux-open-url h4ck3r.me && termux-reload-settings; }
+2line() { rm -rf ~/.zshrc; git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh; cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc; cd ~/Termux-os ; bash os.sh; }
+3line() { pkg install zsh; chsh -s zsh; cd ~/Termux-os ; bash os.sh; }
+4line() { chsh -s bash; cd ~/Termux-os ; bash os.sh; }
+5line() { rm -rf ~/.zshrc; cd ~/Termux-os/.object; bash .2.sh; clear ; cd ~/Termux-os ; bash os.sh; }
+6line() { cd ~/Termux-os/.object; bash .1.sh; clear ; cd ~/Termux-os ; bash os.sh; }
+7line() { cd ~/Termux-os/.object; rm -rf ~/.zshrc; chsh -s zsh; bash .3.sh; clear ; cd ~/Termux-os ; bash os.sh; }
+8line() { rm -rf ~/Termux-os; cd; git clone https://github.com/h4ck3r0/Termux-os; cd ~/Termux-os ; bash os.sh; }
+9line() {
+    echo -e "\n${C}Initialising Security Protocol...${RS}"
+    echo -ne "${Y}Create Access Key: ${RS}"
+    read -s new_pass
+    echo
+    lock_code="#LOCK_START
     clear
-    cd ~/Termux-os/.object/ && cp -r 'ANSI Shadow.flf' $PREFIX/share/figlet/ASCII-Shadow.flf 
-    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-    pkg install toilet figlet exa -y
-    cd ~/Termux-os/.object
-    rm -rf ~/.termux/colors.properties
-    rm -rf /data/data/com.termux/files/usr/etc/motd
-    cp -r .colors.properties ~/.termux/colors.properties
-    cp -r .termux.properties ~/.termux/termux.properties
-    curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf > ~/.termux/font.ttf 
+    echo -e '\033[1;32m'
+    echo '  System check...'
+    sleep 0.2
+    echo '  Encrypted link established.'
+    sleep 0.2
     clear
-    cd ~/Termux-os ; bash os.sh
-    termux-open-url h4ck3r.me && termux-reload-settings
+    attempt=1
+    while [ \$attempt -le 3 ]; do
+        echo -e \"\n${C}╔══════════════════════════════════════╗\"
+        echo -e \"║        ${R}SECURE SHELL ACCESS           ${C}║\"
+        echo -e \"╚══════════════════════════════════════╝${RS}\"
+        echo -ne \"${Y} [Attempt \$attempt/3] Enter Key: ${RS}\"
+        read -s pass_input
+        echo
+        if [ \"\$pass_input\" == \"$new_pass\" ]; then
+            echo -e \"${G} ACCESS GRANTED.${RS}\"
+            sleep 1
+            clear
+            break
+        else
+            echo -e \"${R} DENIED.${RS}\"
+            if [ \$attempt -eq 3 ]; then
+                exit
+            fi
+            ((attempt++))
+        fi
+    done
+    #LOCK_END"
+    echo "$lock_code" >> ~/.bashrc
+    [ -f ~/.zshrc ] && echo "$lock_code" >> ~/.zshrc
+    echo -e "${G}Lock Configured. Your files remain untouched.${RS}"
+    sleep 2
+    menu
 }
 
-2line() {
-    rm -rf ~/.zshrc
-    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-    cd ~/Termux-os ; bash os.sh
+10line() {
+    sed -i '/#LOCK_START/,/#LOCK_END/d' ~/.bashrc
+    [ -f ~/.zshrc ] && sed -i '/#LOCK_START/,/#LOCK_END/d' ~/.zshrc
+    echo -e "${R}Security Protocol Deactivated.${RS}"
+    sleep 2
+    menu
 }
-
-3line() {
-    pkg install zsh
-    chsh -s zsh
-    cd ~/Termux-os ; bash os.sh
-}
-
-4line() {
-    chsh -s bash
-    cd ~/Termux-os ; bash os.sh
-}
-
-5line() {
-    rm -rf ~/.zshrc
-    cd ~/Termux-os/.object
-    bash .2.sh
-    clear ; cd ~/Termux-os ; bash os.sh
-}
-
-6line() {
-    cd ~/Termux-os/.object
-    bash .1.sh
-    clear ; cd ~/Termux-os ; bash os.sh
-}
-
-7line() {
-    cd ~/Termux-os/.object
-    rm -rf ~/.zshrc
-    chsh -s zsh
-    bash .3.sh
-    clear ; cd ~/Termux-os ; bash os.sh
-}
-
-8line() {
-    rm -rf ~/Termux-os
-    cd
-    git clone https://github.com/h4ck3r0/Termux-os
-    cd ~/Termux-os ; bash os.sh
-}
-
 
 menu() {
     banner
@@ -130,9 +113,10 @@ menu() {
     printf "\n${left_pad}${R}[${W}6${R}]${G} Zsh Theme"
     printf "\n${left_pad}${R}[${W}7${R}]${G} Highlight / AutoSuggest"
     printf "\n${left_pad}${R}[${W}8${R}]${G} Update"
-    printf "\n${left_pad}${R}[${W}9${R}]${G} Exit\n\n"
-    
-    echo -ne "${left_pad}${C}Choose Option : ${RS}"
+    printf "\n${left_pad}${R}[${W}9${R}]${B} Add Cyber Lock"
+    printf "\n${left_pad}${R}[${W}10${R}]${R} Remove Lock"
+    printf "\n${left_pad}${R}[${W}0${R}]${G} Exit\n\n"
+    echo -ne "${left_pad}${C}Selection: ${RS}"
     read a
     case $a in
         1) 1line ;;
@@ -143,9 +127,10 @@ menu() {
         6) 6line ;;
         7) 7line ;;
         8) 8line ;;
-        9) exit ;;
-        *) printf "\033[1;91m Invalid input!!!\n"; sleep 1; menu ;;
+        9) 9line ;;
+        10) 10line ;;
+        0) exit ;;
+        *) menu ;;
     esac
 }
-
 menu
