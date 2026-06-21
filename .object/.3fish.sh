22059f7ac3e35e6c5b7f2dc133f20823c93bd5cb
h4ck3r0 <rajaryan2315@gmail.com>
2026-06-21 20:25:40 +0530

    fix: replace hardcoded cd ~/Termux-os/.object with SCRIPT_DIR in all .object scripts

--- .object/.3fish.sh
+++ .object/.3fish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 git clone https://github.com/oh-my-fish/oh-my-fish.git ~/.local/share/omf --depth=1 2>/dev/null || true
 
 clear
@@ -13,7 +14,7 @@ echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[10
 echo
 echo -e "  \e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
 echo ""
-cd ~/Termux-os/.object
+cd "$SCRIPT_DIR"
 echo -e '\e[1;96m'
 read -p '  Type Banner Name ❯ ' name
 sed -e "s/PROC/$name/g" .2fishrc > ~/temp_fish_config.tmp
